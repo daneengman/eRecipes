9cf624de17b6feadd9569753dd6f193302757b94
daneengman <drengman7@gmail.com>
2023-02-04 16:43:35 -0500

    wow

--- eRecipesSite/catalog/models.py
+++ eRecipesSite/catalog/models.py
@@ -31,17 +31,17 @@ class Recipe(models.Model):
     """Model representing a recipe."""
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular recipe across whole library')
 
-    title = models.CharField(max_length=200, default=f'testing {random.random()}')
+    title = models.CharField(max_length=200)
 
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author is a string rather than an object because it hasn't been declared yet in the file
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, default=1) # default is for testing only
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True) # default is for testing only
 
     # need to figure out ingredients and stuff...
     # later fields: ingredients (quantities? uniqueness?) last made, rating, personal notes, pictures??? 
     # uploaded, whatever else
 
-    description = models.TextField(max_length=1000, help_text='Enter a brief description of the recipe', default="testing 213")
+    description = models.TextField(max_length=1000, help_text='Enter a brief description of the recipe', blank=True, null=True)
     # isbn = models.CharField('ISBN', max_length=13, unique=True,
     #                          help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
@@ -86,4 +86,4 @@ class Author(models.Model):
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.last_name}, {self.first_name}'
+        return f'{self.first_name} {self.last_name}'
