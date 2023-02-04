cdfe3dd68d942442b0c3a5058159019b71faa26d
Jackson Daggett <jddag@jacksons-mbp.wifi.local.cmu.edu>
2023-02-04 15:32:30 -0500

    almost completed searching algos

--- Testing/Searching/ingredient_search.py
+++ Testing/Searching/ingredient_search.py
@@ -1,11 +1,9 @@
-import cv2
-import pytesseract
-import pdf2image
 import os
+import json
 
-# Given a string and a list of strings, returns a dictionary with the number of occurences
-# of the 'cleaned-up' string in the list
-def process_word(ingredient, words):
+# extracts specified ingredient from recipe dict as a new singleton 
+# dictionary
+def process_word(ingredient, recipe_dict):
     dict = {}
     
     ingredient_words = ingredient.split()
@@ -14,63 +12,46 @@ def process_word(ingredient, words):
 
     punctuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
-    for word in words:
+    for word in recipe_dict:
         for c in word.lower().strip():
             if c in punctuation:
                 word = word.replace(c, "")
         if(word.lower().strip() in dict):
-            dict[word.lower().strip()] += 1
-    
+            dict[word.lower().strip()] = recipe_dict[word]
     return dict
 
-# Given two string lists, returns a dictionary with the number of occurences
-# of each string from list 1 in list 2
-def process_ingredients(ing_lst, words):
+# create a new dictionary that extracts ingredients from the recipe_dict
+def process_ingredients(ing_lst, recipe_dict):
     dict = {}
-    for ing in ing_lst:
-        dict.update(process_word(ing, words))
-
+    for ingredient in ing_lst:
+        dict.update(process_word(ingredient, recipe_dict))
     return dict
-
-# Takes a string path to a pdf file as input, returns a list containing words in pdf
-def pdf_to_list(path):
-
-    # Initialize local path to tesseract
-    pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract' # can i get around this
-
-    # Save pages of a pdf to a series of pngs, ordered numerically
-    pdf = pdf2image.convert_from_path(path)
-    images = []
-    for i in range(len(pdf)):
-        pdf[i].save(f'cbbr_page{i+1}.png', 'PNG')
-        images.append(cv2.imread(f'cbbr_page{i+1}.png'))
-
-    # Generate a list of words that appear in the saved images
-    words = []
-    for img in images:
-        text = pytesseract.image_to_string(img)
-        words += text.split()
-    
-    return words
 
 # Algorithm for rating dictionary by how well it matches the ingredients_list
 # Higher return values indicate closer matches
 def rate(ingredients_list, dict):
     return sum(dict.values())
 
-# Returns a dictionary where each ingredient in the ingredients string list is mapped to the number of
-# times it occurs in the recipe pdf specified by the string path
+# Takes a list of ingredients and a path to a database, returns
+# a list of paths in the database that contain the recipes
+# that best match the ingredients list
 def main(ingredients_list, db_path):
+
+    # recipes is a list of text files, each containing a dictionary of the words from the corresponding recipe
     recipes = []
     for f in os.listdir(db_path):
         if os.path.isfile(os.path.join(db_path, f)):
             recipes.append(f)
 
-    # Create a list of paths to highest matching pdfs in database to ingredients list
     best = []
     highest_rating = 0
     for recipe_path in recipes:
-        dict = process_ingredients(ingredients_list, pdf_to_list(recipe_path))
+        # Read the dictionary from the text file
+        with open(os.path.join(db_path, recipe_path)) as f:
+            data = f.read()
+        recipe_dict = json.loads(data)
+
+        dict = process_ingredients(ingredients_list, recipe_dict)
         rating = rate(ingredients_list, dict)
         if rating > highest_rating:
             highest_rating = rating
--- Testing/Searching/storepdf.py
+++ Testing/Searching/storepdf.py
@@ -1,3 +1,32 @@
-# Takes the path to a pdf and stores it to directory dir
-def store(pdf, dir):
-    pass
+import cv2
+import pytesseract
+import pdf2image
+import json
+import os
+
+# Takes the path to a pdf and stores it to directory dir as a text file with a dictionary
+def store(pdf_path, dir):
+    
+    # Initialize local path to tesseract
+    pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
+
+    # Save pages of a pdf to a series of pngs, ordered numerically
+    pdf = pdf2image.convert_from_path(pdf_path)
+    images = []
+    for i in range(len(pdf)):
+        pdf[i].save(f'cbbr_page{i+1}.png', 'PNG')
+        images.append(cv2.imread(f'cbbr_page{i+1}.png'))
+
+    # Generate a list of words that appear in the saved images
+    words = []
+    for img in images:
+        text = pytesseract.image_to_string(img)
+        words += text.split()
+
+    d = {word:words.count(word) for word in words}
+
+    filename = (os.path.basename(pdf_path))
+    title = filename[:(len(filename)-4)]
+
+    with open(os.path.join(dir, (title + '.txt')), 'w') as recipe_file:
+        recipe_file.write(json.dumps(d))
